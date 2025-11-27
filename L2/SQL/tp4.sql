ad1fef2ce29915eba12c7636277baafb1ebcf0cc
Vincehhh <104105898+Vincehhh@users.noreply.github.com>
2025-11-27 15:33:11 +0100

    Update tp4.sql

--- L2/SQL/tp4.sql
+++ L2/SQL/tp4.sql
@@ -33,4 +33,52 @@ SELECT v_id AS message;
 END IF;
 END
 $$
+
 DELIMITER;
+
+--2.b
+CALL inscrire_participant(1, 1);
+CALL inscrire_participant(1, 1);
+
+--3
+
+SELECT nom, prenom , personne_id, COUNT(article_id)
+FROM autheur
+GROUP BY personneid,
+nom,
+prenom ;
+
+--3b
+DELIMITER 
+$$ 
+CREATE 
+OR REPLACE FONCTION nb_articles(this_auteur_id) RETURNS INT 
+BEGIN 
+DECLARE 
+v_count INT;
+SELECT
+	COUNT(articleid) INTO v_count
+FROM
+	rediger
+WHERE
+	auteurid = this_auteurs_id;
+RETURN v_count;
+END
+$$
+DELIMITER;
+
+--3c
+
+SELECT nom, prenom , personne_id, CALL nb_articles(personnes_id)
+FROM autheur
+
+
+--4a
+DELIMITER
+$$
+CREATE 
+OR REPLACE TRIGGER congres_date_insertion BEFORE
+INSERT
+	ON congres FOR EACH ROW
+BEGIN
+
