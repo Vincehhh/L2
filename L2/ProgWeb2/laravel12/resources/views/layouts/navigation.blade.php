ee94a7cd2d38ed77cb39d2f01c8ce3eb469e8c26
Vincehhh <bomberbrite04@gmail.com>
2026-01-30 11:34:46 +0100

    add-seeder-navigation

--- L2/ProgWeb2/laravel12/resources/views/layouts/navigation.blade.php
+++ L2/ProgWeb2/laravel12/resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('videos.index')" :active="request()->routeIs('videos.index')">
+                     {{ __('Videos') }}
+    </x-nav-link>
                 </div>
             </div>
 
@@ -82,6 +86,10 @@
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
+                </x-responsive-nav-link>
+
+                <x-responsive-nav-link :href="route('videos.index')" :active="request()->routeIs('videos.index')">
+                {{ __('Videos') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
